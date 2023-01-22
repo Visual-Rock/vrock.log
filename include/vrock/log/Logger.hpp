364d39622bdaa0d72b5d40b64f5416da73a7b564
Visual-Rock <48381849+Visual-Rock@users.noreply.github.com>
2023-01-22 16:28:57 +0100

    VRLIBS-34-35: Logger Manager

--- include/vrock/log/Logger.hpp
+++ include/vrock/log/Logger.hpp
@@ -16,4 +16,19 @@ namespace vrock::log {
 
         std::shared_ptr<spdlog::logger> log;        
     };
+
+    /// @brief creates a logger with a given config
+    /// @param cfg config for the logger
+    /// @return shared pointer containing the logger
+    std::shared_ptr<Logger> create_logger(LoggerConfig cfg);
+
+    /// @brief creates a logger with a given name
+    /// @param n name of the logger
+    /// @return shared pointer containing the logger
+    std::shared_ptr<Logger> create_logger(std::string n);
+
+    /// @brief gets the logger with the given name. if the logger is not found it creates one.
+    /// @param name name of the logger
+    /// @return shared pointer containing the logger
+    std::shared_ptr<Logger> get_logger(std::string name);
 }
