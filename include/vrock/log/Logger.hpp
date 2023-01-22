de0c6858f23141359fed026788006ae63d59ba6f
Visual-Rock <48381849+Visual-Rock@users.noreply.github.com>
2023-01-22 13:09:04 +0100

    VRLIBS-13: Use Config in Logger

--- include/vrock/log/Logger.hpp
+++ include/vrock/log/Logger.hpp
@@ -5,10 +5,14 @@
 
 #include <spdlog/spdlog.h>
 
+#include "LoggerConfig.hpp"
+
 namespace vrock::log {
     class Logger {
     public:
         Logger(std::string n);
+        Logger(LoggerConfig cfg);
+
         std::shared_ptr<spdlog::logger> log;        
     };
 }
--- src/Logger.cpp
+++ src/Logger.cpp
@@ -4,6 +4,7 @@
 #include <unordered_map>
 
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/null_sink.h>
 
 namespace vrock::log {
     Logger::Logger(std::string n)
@@ -13,5 +14,15 @@ namespace vrock::log {
         log = std::make_shared<spdlog::logger>(n, sinks.begin(), sinks.end());
         log->set_level( spdlog::level::info);
         log->flush_on(spdlog::level::info);
+    }
+
+    Logger::Logger(LoggerConfig cfg)
+    {
+        auto sinks = cfg.get_config();
+        if (sinks.empty())
+            sinks.push_back(std::make_shared<spdlog::sinks::null_sink_mt>());
+        log = std::make_shared<spdlog::logger>(cfg.name, sinks.begin(), sinks.end());
+        log->set_level((spdlog::level::level_enum)cfg.level);
+        log->flush_on((spdlog::level::level_enum)cfg.level);
     }
 }
